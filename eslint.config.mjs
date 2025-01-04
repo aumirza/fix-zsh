1aaf769c5d792dc46928dbc76d182d919c48cb15
Ahmadullah mirza <mirzaahmad9028.am@gmail.com>
2025-01-04 12:13:17 +0530

    initialize project with TypeScript, ESLint, Prettier setup

---
+++ eslint.config.mjs
@@ -0,0 +1,22 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import eslintConfigPrettier from "eslint-config-prettier";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ["src/**/*.{js,mjs,cjs,ts}"] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  eslintConfigPrettier,
+  {
+    ignores: [
+      ".husky",
+      "dist",
+      "node_modules",
+      "package-lock.json",
+      "yarn.lock",
+    ],
+  },
+];
